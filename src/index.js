f1cf50f8f5ea27a2b6c55488830a4ccd209cdf51
Dina Cahyanti <124108993+Dinacast@users.noreply.github.com>
2024-11-01 15:12:50 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+const nilai = 70;
+if (nilai < 60) {
+  console.log("maaf, anda tidak lulus");
+} else {
+  console.log("Selamat, anda lulus!");
+}
